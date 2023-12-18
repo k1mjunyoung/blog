9c4a0e9e1e3db857f2e547967a41e0399370f4ff
k1mjunyoung <vovrwm@gmail.com>
2023-12-18 16:49:59 +0900

    Initial commit

---
+++ blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "메모장",
+  description: "안녕하세요. 주니어 개발자 김준영입니다.",
+  author: "김준영",
+  siteUrl: "https://k1mjunyoung.github.io/blog",
+  links: {
+    github: "https://github.com/k1mjunyoung",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com",
+    // twitter: "https://twitter.com",
+    // x: "https://x.com",
+    // instagram: "https://www.instagram.com",
+    // medium: "https://medium.com",
+    // blogger: "https://blogger.com",
+    email: "mailto:me@kimjunyoung.com",
+    resume: "https://kimjunyoung.com",
+    link: "https://kimjunyoung.com",
+  },
+  utterances: {
+    repo: "k1mjunyoung/utterences",
+    type: "pathname",
+  },
+}
